39674b38fe887837d89c86925b1078e103cbe578
Kurtis Moffett <moffkr91@gmail.com>
2018-02-13 10:59:25 -0800

    guess is now creating strings

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,22 @@
-var user_guess;
+
+
+
 
 //Selects a random item from the array
 word_list = ["goldfish", "frog", "snake", "cat"]
+console.log(word_list)
 var current_word = word_list[Math.floor(Math.random() * word_list.length)]
 console.log(current_word)
+//Turns current_word into new string
+var current_array = current_word.split("")
+console.log(current_array)
 
-//Changes the word_list string into a new array
-var word_array = Array.from(current_word)
-console.log(word_array)
 
-//Generates the users choice
-document.onkeyup = function(event) {
-    user_guess = event.key
-    console.log(user_guess)
-};
-
-if (word_array.indexOf === user_guess) {
-    print(match)
+//Generates the user's guess
+document.onkeyup = function guess() {
+    var array = []
+    var item = event.key
+    array.push(item);
+    console.log(array)
 }
-
- 
-   
-
-
-
 
