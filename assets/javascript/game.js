1cdfdca95199a68c37d98a98eb05bbc78ca7a883
Kurtis Moffett <moffkr91@gmail.com>
2018-02-12 18:27:11 -0800

    quicksave

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,25 @@
+var user_guess;
 
 //Selects a random item from the array
 word_list = ["goldfish", "frog", "snake", "cat"]
-console.log(word_list)
 var current_word = word_list[Math.floor(Math.random() * word_list.length)]
 console.log(current_word)
 
+//Changes the word_list string into a new array
+var word_array = Array.from(current_word)
+console.log(word_array)
 
-
-
-var user_guess = document.onkeyup = function(event) {
-    var user_guess = event.key
-console.log(user_guess)
+//Generates the users choice
+document.onkeyup = function(event) {
+    user_guess = event.key
+    console.log(user_guess)
 };
+
+
+if (user_guess === indexOf(word_array)) {
+    alert("It's a match!")
+}
+
+
+
+
