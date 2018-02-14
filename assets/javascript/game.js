013153a8fa6b5f9b7a9f97532bdc0e8520613d6b
Kurtis Moffett <moffkr91@gmail.com>
2018-02-13 21:33:24 -0800

    quicksave

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,31 +31,28 @@
 
 
 //***Stuff that needs to be defines globally
-var user_guess 
+var user_guess = document.getElementById("user-guess-div")
+
 
 
 
 //Set word to guess
 //Selects a random item from the array
 word_list = ["goldfish", "frog", "snake", "cat"]
-console.log(word_list)
 var current_word = word_list[Math.floor(Math.random() * word_list.length)]
+console.log(word_list)
 console.log(current_word)
-//Turns current_word into new string
-var word_array = current_word.split("")
-console.log(word_array)
 
 //Set current word as blanks and set guesses left
 onload = reset_word(), reset_guesses_left()
-function reset_word() {
-    for (var i = 0; i < current_word.length; i++) {
-        document.getElementById("word-blanks").innerHTML= current_word
+    function reset_word() {
+        for (var i = 0; i < current_word.length; i++) {
+            document.getElementById("word-blanks").innerHTML= current_word
+        }
     }
-}
-
-function reset_guesses_left() {
-    document.getElementById("guesses-left").innerHTML= "9"
-}
+    function reset_guesses_left() {
+        document.getElementById("guesses-left").innerHTML= "9"
+    }
 
 
 //Set wrong guesses: document.getElementById("wrong-guesses").innerHTML= "0"
@@ -70,6 +67,3 @@ document.onkeyup = function (event) {
     user_guess_div.innerHTML = user_guess
     console.log(user_guess)
 }
-
- 
-
