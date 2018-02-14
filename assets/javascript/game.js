8bc9c9d9eff30f16c0692e7933a62a6cf333cb10
Kurtis Moffett <moffkr91@gmail.com>
2018-02-13 17:44:35 -0800

    added jQuery href

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 //Set up game
-    //Set word to guess
-    //Set wrong guesses
-    //Set guesses left
-    //Set wins
-    //Set losses
+    //Set word to guess***word-blanks***
+    //Set wrong guesses***wrong-guesses***
+    //Set guesses left****guesses-Left****
+    //Set wins****winCounter***
+    //Set losses***loss-counter***
 
 //Create loop for game
     //Create storable user input
@@ -25,3 +25,19 @@
     //If guesses left equal 0
         //---Add 1 to losses
         //---Restart game with new word***Keep wrong guesses, wins, and losses
+
+
+//Set word to guess
+//Selects a random item from the array
+word_list = ["goldfish", "frog", "snake", "cat"]
+console.log(word_list)
+var current_word = word_list[Math.floor(Math.random() * word_list.length)]
+console.log(current_word)
+//Turns current_word into new string
+var current_array = current_word.split("")
+console.log(current_array)
+
+//Set wrong guesses
+function set_wrong_guesses() {
+    document.getElementById("#wrong-guesses").body.innerHTML="0"
+}
