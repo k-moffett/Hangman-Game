00c03473a36c6c6e04cdebe3a83ae6bc0545225c
Kurtis Moffett <moffkr91@gmail.com>
2018-02-13 17:23:45 -0800

    created new framework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,27 @@
+//Set up game
+    //Set word to guess
+    //Set wrong guesses
+    //Set guesses left
+    //Set wins
+    //Set losses
 
-
-
-
-//Selects a random item from the array
-word_list = ["goldfish", "frog", "snake", "cat"]
-console.log(word_list)
-var current_word = word_list[Math.floor(Math.random() * word_list.length)]
-console.log(current_word)
-//Turns current_word into new string
-var current_array = current_word.split("")
-console.log(current_array)
-
-
-//Generates an array of the user's guesses
-document.onkeyup = function (event) {
-    var user_guess = []
-    var user_input = event.key
-    user_guess.push(user_input);
-    console.log(user_guess)
-}
-
-
-
-  
+//Create loop for game
+    //Create storable user input
+        //Compare user input for letters already chosen
+            //If user input matches a letter already chosen
+                //---Do nothing
+        //Compare user input to letters in chosen word
+            //If user input equals a character
+                //---Replace a blank with the matching character
+        //Compare user input to letters not in chosen word
+            //If user input does not equal a character
+                //---Add 1 to wrong guesses
+                //---Remove 1 from guesses left
+    
+//End current game
+    //If all letters have been guessed
+        //---Add 1 to wins
+        //---Restart game with a new word***Keep wrong guesses, wins, and losses
+    //If guesses left equal 0
+        //---Add 1 to losses
+        //---Restart game with new word***Keep wrong guesses, wins, and losses
