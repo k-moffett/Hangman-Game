bf2329209ba0d7902cd5a89a72659d8b58fe6e98
Kurtis Moffett <moffkr91@gmail.com>
2018-02-13 19:27:38 -0800

    making progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,12 @@
 //Set up game
     //Set word to guess***word-blanks***
-    //Set wrong guesses***wrong-guesses***
     //Set guesses left****guesses-Left****
+
+    //Set wrong guesses***wrong-guesses***
     //Set wins****winCounter***
     //Set losses***loss-counter***
-
+        //^---these three need to only reset when the page refreshes 
+        
 //Create loop for game
     //Create storable user input
         //Compare user input for letters already chosen
@@ -34,10 +36,23 @@ console.log(word_list)
 var current_word = word_list[Math.floor(Math.random() * word_list.length)]
 console.log(current_word)
 //Turns current_word into new string
-var current_array = current_word.split("")
-console.log(current_array)
+var word_array = current_word.split("")
 
-//Set wrong guesses
-function set_wrong_guesses() {
-    document.getElementById("#wrong-guesses").body.innerHTML="0"
+
+//Set current word as blanks
+onload = function restart() {
+    var word_blanks = document.getElementById("word-blanks")
+    for (var i = 0; i < word_array.length; i++) {
+       
+        var new_letter_div = document.createElement("div");
+        new_letter_div.innerHTML = word_array[i];
+        word_blanks.appendChild(new_letter_div);
+    }
+        //Set Guesses left
+            function restart() {
+            document.getElementById("guesses-left").innerHTML= "10"
+        }
 }
+
+//Set wrong guesses: document.getElementById("wrong-guesses").innerHTML= "0"
+//Set wins and losses already at 0 in html
