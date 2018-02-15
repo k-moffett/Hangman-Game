3359d160198be22352c9061820236366cb848149
Kurtis Moffett <moffkr91@gmail.com>
2018-02-15 09:28:32 -0800

    added wrong guesses

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -4,20 +4,16 @@ var game = {
         start: function reset_word() {
             document.getElementById("word-blanks").innerHTML= this.current_word.split("").join(" ")
             this.guesses_left = document.getElementById("guesses-left").innerHTML= "9";
+            this.guesses_left = document.getElementById("wrong-guesses").innerHTML= "0";
         }         
     }
 
 
 function main_loop(params) {
-    var valid_input = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-    var stored_input = []
-   if (event.key != valid_input.indexOf(event.key)) {
-       alert("invalid input")
-   } else {
-    
-        
-    }
+    var valid_input = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
    
+
+
 }
 
 
