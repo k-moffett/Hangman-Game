5d2f825f19d510d71b7891ac5bcd0b48baf3f950
Kurtis Moffett <moffkr91@gmail.com>
2018-02-15 10:31:48 -0800

    can now compare user_guess to valid_input array

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -3,19 +3,21 @@ var game = {
         current_word : word_list[Math.floor(Math.random() * word_list.length)],
         start: function reset_word() {
             document.getElementById("word-blanks").innerHTML= this.current_word.split("").join(" ")
-            this.guesses_left = document.getElementById("guesses-left").innerHTML= "9";
-            this.guesses_left = document.getElementById("wrong-guesses").innerHTML= "0";
+            guesses_left = document.getElementById("guesses-left").innerHTML= "9";
+            wrong_guesses = document.getElementById("wrong-guesses").innerHTML= "0";
         }         
     }
 
 
 function main_loop(params) {
     var valid_input = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-   
-
-
+    var word_array = game.current_word.split().join(" ");
+    var user_input = event.key;
+        for (var i = 0; i <valid_input.length; i++){
+            if (valid_input[i] == user_input)
+            console.log(user_input)
+        }
 }
-
 
 
 
