103e02785ca7e7d2318d591cf97feac344f9c726
Kurtis Moffett <moffkr91@gmail.com>
2018-02-15 09:07:40 -0800

    loop begining to work

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,16 +1,24 @@
 var word_list = ["goldfish", "frog", "snake", "cat"]
 var game = {
         current_word : word_list[Math.floor(Math.random() * word_list.length)],
-        wrong_guesses : 0,
-        guesses_left : 9,
-        wins : 0,
-        losses : 0,
         start: function reset_word() {
-            document.getElementById("word-blanks").innerHTML= this.current_word.split("").join(" ")}
+            document.getElementById("word-blanks").innerHTML= this.current_word.split("").join(" ")
+            this.guesses_left = document.getElementById("guesses-left").innerHTML= "9";
+        }         
     }
 
 
-
+function main_loop(params) {
+    var valid_input = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    var stored_input = []
+   if (event.key != valid_input.indexOf(event.key)) {
+       alert("invalid input")
+   } else {
+    
+        
+    }
+   
+}
 
 
 
@@ -27,4 +35,5 @@ function start_game(params) {
 document.addEventListener("keyup", function (event) {
     console.log(event)
     event.key
+    main_loop()
     });
