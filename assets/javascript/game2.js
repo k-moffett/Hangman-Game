e8d87087e81d24f92e3b4c6f4e40bf9dd809f734
Kurtis Moffett <moffkr91@gmail.com>
2018-02-15 18:25:10 -0800

    got blanks filled in

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -2,10 +2,11 @@ var word_list = ["goldfish", "frog", "snake", "cat"]
 var game = {
         current_word : word_list[Math.floor(Math.random() * word_list.length)],
         start: function reset_word() {
-            document.getElementById("word-blanks").innerHTML= this.current_word.split("").join(" ")
+            document.getElementById("word-blanks").innerHTML= this.current_word.split("").fill("_").join(" ")
             document.getElementById("guesses-left").innerHTML= "9";
           
-        }         
+        }
+               
     }
 
 
@@ -13,20 +14,21 @@ function validate_input(params) {
     var valid_input = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     var user_input = event.key  
         for (var i = 0; i <valid_input.length; i++){
-            if (valid_input[i] == user_input)
-            console.log(user_input)
-            return wrong_guess(user_input)
-        }
-}
+            if (valid_input[i] == user_input){
+                
+        }}}
 
-function wrong_guess(params) {
-    var user_input = params
-    var current_string = game.current_word.split("")
-        for (var i = 0; i <current_string.length; i++){
-            if (current_string[i] != user_input)
-            return alert("wrong")
-        }   
-}
+
+
+
+
+
+
+
+
+
+
+
 
 function add(params) {return x + 1}
 function subtract(params) {return x - 1}
