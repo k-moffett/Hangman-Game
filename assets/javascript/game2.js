893d581f026f830ce6f04c65b69fe7f4c65d7303
Kurtis Moffett <moffkr91@gmail.com>
2018-02-15 19:08:03 -0800

    created used_letter to handle creating an invalid_letters array

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,16 +1,12 @@
 var word_list = ["goldfish", "frog", "snake", "cat"]
 var game = {
         valid_input : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+        invalid_input : [],
         current_word : word_list[Math.floor(Math.random() * word_list.length)],
         start: function reset_word() {
             document.getElementById("word-blanks").innerHTML= this.current_word.split("").fill("_").join(" ")
-            document.getElementById("guesses-left").innerHTML= "9";
-          
-        }
-               
-    }
-
-
+            document.getElementById("guesses-left").innerHTML= "9"; 
+        }}
 function validate_input(params) {
     var user_input = event.key  
         for (var i = 0; i <game.valid_input.length; i++){
@@ -18,9 +14,9 @@ function validate_input(params) {
                 used_letters(game.valid_input, user_input)      
         }}}
 function used_letters(valid_input, user_input) {
-    console.log(valid_input.indexOf(user_input));
+     var used_letter = (valid_input.indexOf(user_input))
+     console.log(used_letter)
 }
-
 
 
 
