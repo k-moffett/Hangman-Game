b29ae61e9601db2ae75ba11a095faea0f54a4710
Kurtis Moffett <moffkr91@gmail.com>
2018-02-16 20:27:19 -0800

    restart game and losses functions are the only things left to fix

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,9 @@ var word_list = ["goldfish", "frog", "snake", "cat"]
 var game = {
         valid_input : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
         current_word : word_list[Math.floor(Math.random() * word_list.length)],
-        start: function reset_word() {
-            word_blanks = document.getElementById("word-blanks").innerHTML= this.current_word.split("").fill("_").join(" ")
-            document.getElementById("wrong-guesses").innerHTML= "0";
+        start : function reset_word() {
+            document.getElementById("word-blanks").innerHTML= this.current_word.split("").fill("_").join(" ")
             document.getElementById("guesses-left").innerHTML= "9";
-            document.getElementById("win-counter").innerHTML= "0";
-            document.getElementById("loss-counter").innerHTML= "0"; 
 }}
 function validate_input(params) {
     var user_input = event.key  
@@ -27,21 +24,9 @@ function remove_used_letters(used_letter, user_input) {
 function incorrect_guess(user_input) {
         for (var i = 0; i <game.current_word.split("").length; i++){
             if (game.current_word.split("")[i] == user_input){
-                correct_guess(user_input) 
+                correct_guess(user_input)
+//            }else{ return not_good() 
 }}}
-
-
-
-
-function not_good(user_input) {
-    var wrong_number = document.getElementById("wrong-guesses").innerHTML
-    var guesses_number = document.getElementById("guesses-left").innerHTML
-}
-
-
-
-
-
 function correct_guess(user_input) {
 var index = game.current_word.indexOf(user_input)
 var current_word = document.getElementById("word-blanks").innerHTML
@@ -50,7 +35,6 @@ var new_word = split_word.splice(index, 1, user_input)
 document.getElementById("word-blanks").innerHTML= split_word.join(" ")
 fill_blanks()
 }
-
 function fill_blanks() {
 var word = document.getElementById("word-blanks").innerHTML
 var your_word = word.split(" ")
@@ -63,13 +47,36 @@ var correct_final_word = correct_word.join(" ")
 console.log(your_final_word)
 console.log(correct_final_word)
 if (your_final_word === correct_final_word) {
-    alert("you win!")
+    add_wins()
+}}
+function add_wins (){
+var number = 0
+var point = 1
+var score = number + point 
+document.getElementById("win-counter").innerHTML= score
+console.log(score)
+start_game()
 }
-}
-
-
-    
-    
+//function not_good() {
+ //   var number = document.getElementById("guesses-left").innerHTML
+  //  var wrong_guesses = document.getElementById("wrong-guesses").innerHTML
+  //  var point = 1
+ //   var guesses = number - point
+  //  var false_guesses = wrong_guesses + point
+  //  document.getElementById("guesses-left").innerHTML= guesses
+  //  document.getElementById("wrong-guesses").innerHTML= false_guesses
+  //  lose(guesses)
+//    } 
+//function lose(guesses) {
+//console.log(guesses)
+//if (guesses <= 0) {
+//var number = 0
+//var point = 1
+//var score = number - point 
+//document.getElementById("loss-counter").innerHTML= score
+//start_game()
+//}}  
+   
 
 
 
