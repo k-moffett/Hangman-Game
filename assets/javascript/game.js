193489ce7dac20a60dcdc89f83e7b278513847ab
Kurtis Moffett <moffkr91@gmail.com>
2018-02-19 12:39:42 -0800

    actually working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,175 @@
-var word_list = ["goldfish", "frog", "snake", "cat"]
-var game = {
-        valid_input : ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-        current_word : word_list[Math.floor(Math.random() * word_list.length)],
-        start : function reset_word() {
-            document.getElementById("word-blanks").innerHTML= this.current_word.split("").fill("_").join(" ")
-            document.getElementById("guesses-left").innerHTML= "9";
-}}
-function validate_input(params) {
-    var user_input = event.key  
-        for (var i = 0; i <game.valid_input.length; i++){
-            if (game.valid_input[i] == user_input){
-                used_letters(game.valid_input, user_input)      
-}}}
-function used_letters(valid_input, user_input) {
-    var used_letter = (valid_input.indexOf(user_input))
-    remove_used_letters(used_letter, user_input)
+
+//I wouldn't recommend changing these names. These id names match the layout if you change these names here make sure you change it on your html page as well.
+var ids = ['hangmanWord','wrongGuesses','guessesLeft','winCounter','lossCounter'];
+
+//finish declaring the rest of the variables we need. 
+///**** Hint use the names in the ids array to finish the last 3 variables we need
+var answerWord= ''
+var winCounter = 0;
+var lossCounter= 0;
+
+//add missing 3 variables named
+var hangmanWord= '';
+var wrongGuesses= [];
+var guessesLeft= 0
+//Add how many names you want to this word bank. All names need to be lowercase.
+var wordBank= ["cat", "horse", "rat", "chicken", "bat", "goldfish", "goat",]
+
+//Valid characters we want to accept
+var validLetters = ["a", 'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+//Randomize and return
+function randomize(arr){
+    // [0,1,2,3,4].length = 5
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
 }
-function remove_used_letters(used_letter, user_input) {
-    var remove_this = used_letter
-    var new_valid_letters = game.valid_input.splice(remove_this, 1)
-    incorrect_guess(user_input)
+
+function updateLetters(letter){
+    for( var i=0; i<answerWord.length; i++){
+        //the for loop runs through all of the words letters
+        // now if a letter matches, the if (below) comes in to play
+        if(letter === answerWord[i]) {
+
+            hangmanWord[i] =answerWord[i];
+        }
+    }
 }
-function incorrect_guess(user_input) {
-        for (var i = 0; i <game.current_word.split("").length; i++){
-            if (game.current_word.split("")[i] == user_input){
-                correct_guess(user_input)
-//            }else{ return not_good() 
-}}}
-function correct_guess(user_input) {
-var index = game.current_word.indexOf(user_input)
-var current_word = document.getElementById("word-blanks").innerHTML
-var split_word = current_word.split(" ")
-var new_word = split_word.splice(index, 1, user_input)
-document.getElementById("word-blanks").innerHTML= split_word.join(" ")
-fill_blanks()
+//Explain what the updateLetters function is doing.
+//The for loop checks each letter within answerWord to see if it matches the letter given to the function
+//If it matches, then a hidden character will become the letter entered
+
+
+
+
+
+function updateGuesses(letter){
+    //Write the code needed to 
+    //add this letter to the wrong guesses array
+    wrongGuesses.push(letter)
+
+    //subtract 1 from the guesses left number
+    guessesLeft--
 }
-function fill_blanks() {
-var word = document.getElementById("word-blanks").innerHTML
-var your_word = word.split(" ")
-var correct_word = game.current_word.split("")
-win(your_word, correct_word)            
+
+function hideWord(arr) {
+    //Please fill in what we want our array to be full of. You can add any symbol you want right now we have it full of *. This should be what the user sees when it is a brand new fresh game.
+    return Array(arr.length).fill("_") 
 }
-function win(your_word, correct_word){
-var your_final_word = your_word.join(" ")
-var correct_final_word = correct_word.join(" ")
-console.log(your_final_word)
-console.log(correct_final_word)
-if (your_final_word === correct_final_word) {
-    add_wins()
-}}
-function add_wins (){
-var number = 0
-var point = 1
-var score = number + point 
-document.getElementById("win-counter").innerHTML= score
-console.log(score)
-start_game()
+
+//The key is the user's inputted key.
+function validateInput(inputKey) {
+    // Whenever a return is executed it breaks out of the function. 
+    // So if validLetters.indexOf(key) === -1 is true then i would return false. This prevents the rest of the code inside this code block from running.
+    // This condition Makes sure an a-z key is pressed.
+    // if not it returns a value of -1 because -1 is not a valid value
+    if(validLetters.indexOf(inputKey) === -1) return false;
+
+    // This condition Makes sure key cant be guessed again.
+    // makes sure that any indexOf value that is 0  and above (example 0=a, 1=b...) does not get used again.
+    if(wrongGuesses.indexOf(inputKey) >= 0) return false;
+
+    // This condition Makes sure hangman keys cant be pressed again
+    // makes sure that any indexOf value that is 0  and above (example 0=a, 1=b...) does not get used again. 
+    if(hangmanWord.indexOf(inputKey) >= 0) return false;
+    return true;
 }
-//function not_good() {
- //   var number = document.getElementById("guesses-left").innerHTML
-  //  var wrong_guesses = document.getElementById("wrong-guesses").innerHTML
-  //  var point = 1
- //   var guesses = number - point
-  //  var false_guesses = wrong_guesses + point
-  //  document.getElementById("guesses-left").innerHTML= guesses
-  //  document.getElementById("wrong-guesses").innerHTML= false_guesses
-  //  lose(guesses)
-//    } 
-//function lose(guesses) {
-//console.log(guesses)
-//if (guesses <= 0) {
-//var number = 0
-//var point = 1
-//var score = number - point 
-//document.getElementById("loss-counter").innerHTML= score
-//start_game()
-//}}  
-   
 
+function takeTurn(letter){
+    //Add an if/else statement here.
+    //if the letter is in the answerWord i should see a call to the updateLetters function
+    if (answerWord.indexOf(letter)>=0) {
+        updateLetters(letter)
+    } else {
+        updateGuesses(letter)
+    }
+    //otherwise i should see a call to the the updateGuesses function
 
-
-
-
-
-
-function start_game(params) {
-    game.start()
+    // keep this line here
+    print(ids);
 }
-document.addEventListener("keyup", function (event) {
-    event.key
-    validate_input()
+
+function updateScore(){
+    //Add an if/else statement here.
+    //if the user wins i should see a call to updateWin
+    if (hangmanWord.indexOf("_")===-1){
+        updateWin()
+    }
+
+    //or if the user loses i should see a call to the updateLoss
+    else if (guessesLeft===0){
+        updateLoss()
+    }
+}
+
+function updateWin() {
+    //add what happens if the user wins code here
+    winCounter = winCounter+1
+    //Make a call to the startGame function here. 
+    //This call is here to start the game once a player wins.
+    print(document.getElementById("message"), startGame())
+
+}
+
+function updateLoss() {
+    //add what happens if the user loses code here
+    lossCounter = lossCounter++
+    //Make a call to the startGame function here.
+    //This call is here to start the game once a player losses.
+    print("message", startGame())
+}
+//This function takes an arr for an argument (we use it for our id array)
+function print(arr) {
+    arr.forEach(function (idName) {
+
+        // If the idName variable is an array do this
+        if(Array.isArray(window[idName])) {
+            document.getElementById(idName).textContent = window[idName].join(' ')
+        }
+
+        // If the idName variable is any other type besides array do this
+        else {
+            document.getElementById(idName).textContent = window[idName];
+        }
     });
+}
+//explain what the print function is doing.
+//This function is making sure that the variable is a string, then printing it to the DOM
+
+
+
+
+
+
+function startGame() {
+    /* 
+    Add your board setup functions and or code here
+    The wrongGuesses variable needs to be an array
+    The hangmanWord variable needs to be an array
+    The guessesLeft variable needs to be an Integer
+    The winCounter variable needs to be an Integer
+    The lossCounter variable needs to be an Integer
+    */
+   wrongGuesses = []
+   answerWord = randomize(wordBank)
+   hangmanWord = hideWord(answerWord)
+   guessesLeft = 9
+   print(ids)
+}
+
+//keyup event
+//This is how our player is interacting with our game.
+//This drives all the game mechanics.
+document.addEventListener('keyup', function(event){
+    if(validateInput(event.key)){
+        //make a call to the takeTurn Function
+        
+        takeTurn(event.key)
+        
+        //make a call to the updateScore function
+        updateScore()
+
+    };
+});
+//This makes the first call (initial) call to start the game.
+startGame();
